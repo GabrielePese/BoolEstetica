8267a968f4dd9370c1f6fb55ef3f75aac98c4a13
Gabriele <gabrielepese@gmail.com>
2021-01-12 14:34:31 +0100

    id

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,7 +29,7 @@ class HomeController extends Controller{
         $user = User::all();
         $service = DB::table('services')
         ->where ( 'promotion' , '=', '1' )
-        ->orderBy('created_at', 'asc')
+        ->orderBy('id')
          ->get();
         
         $serviceAll = Service::all() ->where ( 'promotion' , '=', '1' );
