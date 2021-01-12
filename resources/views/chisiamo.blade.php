704a5564acdff874721dc8a6355b613d260f2c6d
Gabriele <gabrielepese@gmail.com>
2021-01-12 17:15:37 +0100

    chisiamookcss

--- resources/views/chisiamo.blade.php
+++ resources/views/chisiamo.blade.php
@@ -4,7 +4,7 @@
     
 <div class="container">
     <div class="row">
-        <div class="col-8 mx-auto my-5 mt-5 chisiamo">
+        <div class="col-8 mx-auto chisiamo">
             <h1 class="text-center" style="font-family: 'great vibes'; font-size: 65px;">Chi siamo</h1>
 
         </div>
@@ -14,19 +14,19 @@
           <h2 class="text-center">Debora</h2>
         </div>
     </div>
-    <div class="row my-5">
+    <div class="row my-5 d-flex align-items-center">
         <div class=".col-12 col-md-6">
              <div class="contenitoreImg mx-auto mb-5">
                  <img src="https://i.ibb.co/pzyGKC2/estetista-1728x800-c.jpg" alt="foto1">
              </div>
         </div>
-        <div class=".col-12 col-md-6 mt-3">
+        <div class=".col-12 col-md-6">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates accusamus, ex pariatur iusto aliquam, dolores atque vel laborum ipsa eum qui voluptatibus ut? Magni a enim perspiciatis. Repudiandae, iusto odit.
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, temporibus voluptatibus expedita omnis est nostrum sint deserunt neque? Alias atque maiores praesentium quasi incidunt. Inventore accusantium nobis illum. Sint, quidem?
         </div>
     </div>
 
-    <div class="row ">
+    <div class="row d-flex align-items-center">
         <div class="col-8 mx-auto">
             <h2 class="text-center">Antonella</h2>
           </div>
