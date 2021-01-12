75c77a0ed78b53b9232d6e9ba3b0bd0171a3a687
Gabriele <gabrielepese@gmail.com>
2021-01-12 17:01:34 +0100

    chi siamo css ok

--- resources/views/chisiamo.blade.php
+++ resources/views/chisiamo.blade.php
@@ -4,7 +4,7 @@
     
 <div class="container">
     <div class="row">
-        <div class="col-8 mx-auto my-5">
+        <div class="col-8 mx-auto my-5 mt-5">
             <h1 class="text-center" style="font-family: 'great vibes'; font-size: 65px;">Chi siamo</h1>
 
         </div>
@@ -16,7 +16,7 @@
     </div>
     <div class="row my-5">
         <div class=".col-12 col-md-6">
-             <div class="contenitoreImg">
+             <div class="contenitoreImg mx-auto">
                  <img src="https://i.ibb.co/pzyGKC2/estetista-1728x800-c.jpg" alt="foto1">
              </div>
         </div>
@@ -37,7 +37,7 @@
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, temporibus voluptatibus expedita omnis est nostrum sint deserunt neque? Alias atque maiores praesentium quasi incidunt. Inventore accusantium nobis illum. Sint, quidem?
         </div>
         <div class=".col-12 col-md-6">
-            <div class="contenitoreImg">
+            <div class="contenitoreImg mx-auto">
                 <img src="https://i.ibb.co/pzyGKC2/estetista-1728x800-c.jpg" alt="foto2">
             </div>    
         </div>
