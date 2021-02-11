404c98f20143ddb48b5a26470ce7251a10fe797d
Gabriele <gabrielepese@gmail.com>
2021-02-11 15:53:49 +0100

    gmail stmp

--- app/Http/Controllers/LoggedController.php
+++ app/Http/Controllers/LoggedController.php
@@ -319,7 +319,7 @@ class LoggedController extends Controller
         
 
 
-        Mail::to('gabrielepese@gmail.com')
+        Mail::to('prenotazioneTrattamento@gmail.com')
         ->send(new TrattamentoPrenotato($utente,$datagiorno,$dataorario,$orarioMail,$servizio));
 
        
